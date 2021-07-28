efefb4aec68e73a9224e0a02243bc312fcf877b6
covidanalysis_server <covidanalysis_server>
2021-07-28 22:12:18 +0000

    correcting cohorts issue, and adding comparison plot psy/non-psy diagnoses

--- R/readInputFiles.R
+++ R/readInputFiles.R
@@ -17,63 +17,42 @@
 #'              )
 #' @export readInputFiles
 
-readInputFiles <- function( path, separator = ",", skip = 0, verbose = FALSE ){
-  
-  #set the working directory
-  setwd(path)
+readInputFiles <- function( path, separator = ",", skip = 0, verbose = FALSE, ... ){
   
   #check that the input files needed are in the path 
   if( verbose == TRUE){
     print('Checking if the files are located in the directory provided')
   }
   
-  filesInDirectory <- list.files( path = "./")
-  
-  #for this analysis we need two of the files, patientSummary and patientObservation
-  checkPatientSummary <- ifelse( "localpatientsummary.csv" %in% tolower( filesInDirectory ), "OK", "File missing")
-  
-  if(checkPatientSummary == "OK"){
-    if(verbose== TRUE){
-      print("LocalPatientSummary.csv file in directory")
+  filesInDirectory <- list.files(path = path)
+  fourcefile_names <- c(
+    "LocalPatientSummary.csv",
+    "LocalPatientObservations.csv",
+    "LocalPatientClinicalCourse.csv"
+  )
+  check_files <- fourcefile_names %in% filesInDirectory
+  if (all(check_files)) {
+    if (verbose) {
+      cat("All of ", fourcefile_names, " are in the directory")
     }
-  }else{
-    print("LocalPatientSummary.csv file not present in the directory. Please check if the file name and the 
-                                       directory are correct")
+  } else {
+    cat("Following files not found in the file directory:\n")
+    cat(fourcefile_names[!check_files])
+    cat("Please check if the file name and the directory are correct")
     stop()
   }
-  
-  checkPatientObservations <- ifelse( "localpatientobservations.csv" %in% tolower( filesInDirectory ), "OK", "File missing")
-  
-  if(checkPatientObservations == "OK"){
-    if(verbose== TRUE){
-      print("LocalPatientObservations.csv file in directory")
-    }
-  }else{
-    print("LocalPatientObservations.csv file not present in the directory. Please check if the file name and the 
-                                       directory are correct")
-    stop()
-  }
-  
-  checkPatientClinicalCourse <- ifelse( "localpatientclinicalcourse.csv" %in% tolower( filesInDirectory ), "OK", "File missing")
-  
-  if(checkPatientClinicalCourse == "OK"){
-    if(verbose== TRUE){
-      print("LocalPatientClinicalCourse.csv file in directory")
-    }
-  }else{
-    print("LocalPatientClinicalCourse.csv file not present in the directory. Please check if the file name and the 
-                                       directory are correct")
-    stop()
-  }
-  
+
   #read the files 
   if( verbose == TRUE){
-    print( 'Reading LocalPatientSummary, LocalPatientObservations and LocalPatientClinicalCourse files')
+    cat( 'Reading \n')
+    cat(fourcefile_names)
+    cat(" files")
   }
   
-  patientSummary <- read.delim("LocalPatientSummary.csv", sep = separator, skip = skip)
-  patientObservations <- read.delim("LocalPatientObservations.csv", sep = separator, skip = skip)
-  patientClinicalCourse <- read.delim("LocalPatientClinicalCourse.csv", sep = separator, skip = skip)
+  read_delim_4ce <- function(file_name, ...) read.delim(file.path(path, file_name), sep = separator, skip = skip, ...)
+  patientSummary <- read_delim_4ce("LocalPatientSummary.csv")
+  patientObservations <- read_delim_4ce("LocalPatientObservations.csv")
+  patientClinicalCourse <- read_delim_4ce("LocalPatientClinicalCourse.csv")
   
   if( verbose == TRUE){
     print( paste0( "LocalPatientsummary file contains: ", nrow( patientSummary ), " rows and ", ncol( patientSummary ), " columns."))
@@ -82,10 +61,13 @@ readInputFiles <- function( path, separator = ",", skip = 0, verbose = FALSE ){
   }
   
   #return it as a list
-  files <- list( patientSummary, patientObservations, patientClinicalCourse )
+  files <- list("patientSummary"        = patientSummary,
+                "patientObservations"   = patientObservations,
+                "patientClinicalCourse" = patientClinicalCourse
+  )
   
   if( verbose == TRUE){
-    print( "A list wit the three data.frames read is being generated")
+    print( "A list with the three data.frames read is being generated")
   }
   
   return( files )
